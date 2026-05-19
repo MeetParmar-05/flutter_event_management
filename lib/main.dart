224331094f728d425422f3a2bb6afb65ea9ed7ad
MeetParmar-05 <pragnaparmar.rajkot@gmail.com>
2026-05-19 15:11:06 +0530

    updated splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,10 @@ import 'package:event_management/Widgets/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: SplashScreen())));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: SplashScreen()),
+    ),
+  );
 }
